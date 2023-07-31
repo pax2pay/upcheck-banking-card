da3d456314600934ab40ed405191a820b0deee58
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-07-31 09:59:22 +0200

    Revert "Create Card. (#3)"
    This reverts commit ccdb9edbdbf170694f19fa46770b82f395f1cf56.

--- index.spec.ts
+++ index.spec.ts
@@ -1,22 +1,8 @@
-import "isomorphic-fetch"
-import { pax2pay } from "@pax2pay/model-banking"
-import * as dotenv from "dotenv"
+import "jest"
+import * as library from "./index"
 
-dotenv.config()
-
-const creatable: pax2pay.Card.Creatable = {
-	account: "jJOCImei",
-	details: { expiry: [26, 12], holder: "Upcheck", iin: "411111" },
-	limit: ["GBP", 1000],
-	preset: "p2p-ta-pg-200",
-	rules: [],
-}
-const client = process.env.url && process.env.key ? pax2pay.Client.create(process.env.url, process.env.key) : undefined
-client && (client.realm = "test")
-client && (client.organization = "Y2TgAgLN")
-
-describe("pax2pay.Card", () => {
-	it("create", async () => {
-		expect(pax2pay.Card.is(await client?.cards.create(creatable))).toBeTruthy()
+describe("library", () => {
+	it("a", () => {
+		expect(library.a).toEqual(42)
 	})
 })
