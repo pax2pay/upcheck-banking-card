fd592ff94313d974d5048c2fa3ade79ca0ad31f4
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-11-07 14:46:54 +0100

    Added variety to the succeeding and failing authorizations. (#21)

--- authorization.spec.ts
+++ authorization.spec.ts
@@ -18,15 +18,18 @@ pax2payClient && (pax2payClient.organization = "agpiPo0v")
 describe("pax2pay.Authorization", () => {
 	let authorization: pax2pay.Authorization | undefined
 	let failedAuthorization: pax2pay.Authorization | undefined
+	const now = isoly.DateTime.now()
 	beforeAll(async () => {
-		const yesterday = isoly.DateTime.previous(isoly.DateTime.now(), { hours: 1 })
+		const yesterHour = isoly.DateTime.previous(now, { hours: 1 })
 		const card = await pax2payClient?.cards
 			.list()
 			.then(r =>
 				gracely.Error.is(r)
 					? undefined
-					: r.find(e => e.created < yesterday && e.organization == pax2payClient.organization)?.id
+					: r.find(e => e.created < yesterHour && e.organization == pax2payClient.organization)?.id
 			)
+		const currentMinute = isoly.DateTime.getMinute(now)
+		const creatable = successes[~~(currentMinute / (60 / successes.length))]
 		authorization = await client?.post<pax2pay.Authorization>("/authorization", { ...creatable, card })
 		await client?.post<pax2pay.Authorization>("/authorization", {
 			...creatable,
@@ -34,7 +37,10 @@ describe("pax2pay.Authorization", () => {
 			amount: [creatable.amount[0], -creatable.amount[1]],
 		}) // This is to revert the effect the authorization has on the account.
 		console.log("authorization", authorization)
-		failedAuthorization = await client?.post<pax2pay.Authorization>("/authorization", { ...failedCreatable, card })
+		failedAuthorization = await client?.post<pax2pay.Authorization>("/authorization", {
+			...fails[~~(currentMinute / (60 / fails.length))],
+			card,
+		})
 	})
 	it("create succeeded", () => {
 		expect(pax2pay.Authorization.is(authorization))
@@ -45,39 +51,153 @@ describe("pax2pay.Authorization", () => {
 	})
 })
 
-const creatable: Omit<pax2pay.Authorization.Creatable, "card" | "reference"> = {
-	amount: ["USD", 1],
-	merchant: {
-		name: "Merchant",
-		id: "abcd1234",
-		category: "4511",
-		country: "GB",
-		city: "upcheck town",
-		zip: "12345",
-		address: "Streetname 1, 12345 Towncity",
+// succeeding authorizations, but can be flagged
+export const successes: Omit<pax2pay.Authorization.Creatable, "card" | "reference">[] = [
+	{
+		amount: ["USD", 1],
+		merchant: {
+			name: "paxair",
+			id: "abcd1234",
+			category: "4511",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "An upcheck test authorization, to succeed",
 	},
-	acquirer: {
-		id: "2345erty",
-		number: "1351858913568",
-		country: "GB",
+	{
+		amount: ["USD", 501],
+		merchant: {
+			name: "paxair",
+			id: "abcd1234",
+			category: "4511",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "An upcheck test authorization, to succeed",
 	},
-	description: "An upcheck test authorization, to succeed",
-}
-const failedCreatable: Omit<pax2pay.Authorization.Creatable, "card" | "reference"> = {
-	amount: ["USD", 1],
-	merchant: {
-		name: "Merchant",
-		id: "abcd1234",
-		category: "4511",
-		country: "KP",
-		city: "upcheck town",
-		zip: "12345",
-		address: "Streetname 1, 12345 Towncity",
+	{
+		amount: ["USD", 801],
+		merchant: {
+			name: "paxair",
+			id: "abcd1234",
+			category: "4511",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "An upcheck test authorization, to succeed",
 	},
-	acquirer: {
-		id: "2345erty",
-		number: "1351858913568",
-		country: "GB",
+]
+
+const fails: Omit<pax2pay.Authorization.Creatable, "card" | "reference">[] = [
+	{
+		amount: ["USD", 1],
+		merchant: {
+			name: "paxair",
+			id: "abcd1234",
+			category: "4511",
+			country: "KP",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "Authorization to fail on the realm level due to sanctioned merchant country.",
 	},
-	description: "An upcheck test authorization, to fail",
-}
+	{
+		amount: ["USD", 2000],
+		merchant: {
+			name: "paxair",
+			id: "abcd1234",
+			category: "4511",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "Authorization to fail on the organization level due to very high amount.",
+	},
+	{
+		amount: ["USD", 2600],
+		merchant: {
+			name: "paxair",
+			id: "abcd1234",
+			category: "4511",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "Authorization to fail on the realm level due to very high amount.",
+	},
+	{
+		amount: ["USD", 15],
+		merchant: {
+			name: "paxsino",
+			id: "abcd1234",
+			category: "7801",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "Authorization to fail due to gambling MCC.",
+	},
+	{
+		amount: ["USD", 15],
+		merchant: {
+			name: "pax dog and horse racing",
+			id: "abcd1234",
+			category: "7802",
+			country: "GB",
+			city: "upcheck town",
+			zip: "12345",
+			address: "Streetname 1, 12345 Towncity",
+		},
+		acquirer: {
+			id: "2345erty",
+			number: "1351858913568",
+			country: "GB",
+		},
+		description: "Authorization to fail due to gambling MCC.",
+	},
+]
