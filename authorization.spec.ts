9b13f28f2747237c0f990f230bfea36888431926
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-02-12 15:05:10 +0100

    Parallelized tests.

--- authorization.spec.ts
+++ authorization.spec.ts
@@ -2,7 +2,7 @@ import "isomorphic-fetch"
 import { pax2pay } from "@pax2pay/model-banking"
 import { http } from "cloudly-http"
 import * as dotenv from "dotenv"
-import { Authorization } from "./Authorizaiton"
+import { Authorization } from "./Authorization"
 import { Clients } from "./Clients"
 
 dotenv.config()
@@ -12,26 +12,30 @@ let login: boolean | undefined
 let authorization: Partial<Record<Authorization.Authorizations, pax2pay.Authorization>> | undefined
 describe("pax2pay.Authorization", () => {
 	beforeAll(async () => {
+		const now = Date.now() / 1000
 		client = await Clients.create()
+		console.log("await Clients.create()", Date.now() / 1000 - now)
 		login = client && (await Clients.login(client))
+		console.log("login", Date.now() / 1000 - now)
 		authorization =
 			client.paxgiro && client.pax2payClient && (await Authorization.create(client.paxgiro, client.pax2payClient))
+		console.log("pax2pay.Authorization", Date.now() / 1000 - now)
 	})
 	it("get token", async () => {
 		expect(login).toBeTruthy()
 	})
 	it("create succeeding", () => {
-		console.log("succeeding", JSON.stringify(authorization?.succeeding, null, 2))
+		// console.log("succeeding", JSON.stringify(authorization?.succeeding, null, 2))
 		expect(pax2pay.Authorization.is(authorization?.succeeding))
 		expect(authorization?.succeeding?.status).toEqual("approved")
 	})
 	it("create flagless", () => {
-		console.log("flagless", JSON.stringify(authorization?.flagless, null, 2))
+		// console.log("flagless", JSON.stringify(authorization?.flagless, null, 2))
 		expect(pax2pay.Authorization.is(authorization?.flagless))
 		expect(authorization?.flagless?.status).toEqual("approved")
 	})
 	it("create failing", () => {
-		console.log("failing", JSON.stringify(authorization?.failing, null, 2))
+		// console.log("failing", JSON.stringify(authorization?.failing, null, 2))
 		expect(authorization?.failing?.status).not.toEqual("approved")
 	})
 })
