5b4a3bbfaa4eb1fa47297855c5c4727f83f3b4c0
SiFalorSum <nilshansander@hotmail.com>
2025-05-06 16:17:47 +0200

    Cleanup and update dependencies

--- authorization.spec.ts
+++ authorization.spec.ts
@@ -3,7 +3,7 @@ import { Authorization } from "./Authorization"
 import { Clients, clients } from "./Clients"
 
 let client: Clients | undefined
-let authorization: Partial<Record<Authorization.Authorizations, pax2pay.Authorization>> | undefined
+let authorization: pax2pay.Authorization | undefined
 describe("pax2pay.Authorization", () => {
 	beforeAll(async () => {
 		client = await clients
@@ -12,20 +12,9 @@ describe("pax2pay.Authorization", () => {
 	it("get token", async () => {
 		expect(client?.token).toBeTruthy()
 	})
-	it("create succeeding", () => {
-		authorization?.succeeding?.status !== "approved" &&
-			console.log("succeeding", JSON.stringify(authorization?.succeeding, null, 2))
-		expect(pax2pay.Authorization.type.is(authorization?.succeeding))
-		expect(authorization?.succeeding?.status).toEqual("approved")
+	it("create authorization", () => {
+		authorization?.status !== "approved" && console.log("succeeding", JSON.stringify(authorization, null, 2))
+		expect(pax2pay.Authorization.type.is(authorization))
+		expect(authorization?.status).toEqual("approved")
 	})
-	// it("create failing", () => {
-	// 	authorization?.failing?.status === "approved" &&
-	// 		console.log("failing", JSON.stringify(authorization?.failing, null, 2))
-	// 	expect(authorization?.failing?.status).not.toEqual("approved")
-	// })
-	// it("create credit", () => {
-	// 	authorization?.credit?.status !== "approved" &&
-	// 		console.log("credit", JSON.stringify(authorization?.credit, null, 2))
-	// 	expect(authorization?.credit?.status).toEqual("approved")
-	// })
 })
