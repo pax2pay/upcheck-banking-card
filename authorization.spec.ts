58d87181f75cc6ea85c725b8badcf2c67510bafc
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2026-01-13 10:52:20 +0100

    Fixed checking for timeout. Added timestamp logs for authorization tests. (#68)

--- authorization.spec.ts
+++ authorization.spec.ts
@@ -6,8 +6,13 @@ let client: Clients | undefined
 let authorization: pax2pay.Authorization | undefined
 describe("pax2pay.Authorization", () => {
 	beforeAll(async () => {
+		console.log("pax2pay.Authorization.beforeAll started.")
+		const start = performance.now()
 		client = await clients
+		const clientsAwaitedTime = performance.now() - start
+		console.log("pax2pay.Authorization.beforeAll awaiting clients took: ", clientsAwaitedTime)
 		authorization = client.paxgiro && client.pax2pay && (await Authorization.create(client.paxgiro, client.pax2pay))
+		console.log("pax2pay.Authorization.beforeAll authorization creation took: ", performance.now() - clientsAwaitedTime)
 	})
 	it("get token", async () => {
 		expect(client?.token).toBeTruthy()
